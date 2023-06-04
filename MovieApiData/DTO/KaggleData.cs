c4363afcca1584bc25eeee10f74afd8f9d56134c
malsoft <github@malsoft.com>
2023-06-05 00:07:35 +0100

    Defines a DTO type for the incoming data from the Kaggle dataset

---
+++ MovieApiData/DTO/KaggleData.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.DTO;
+
+public class KaggleData :
+	IProduction,
+	IPopularity,
+	IPublicity
+{
+	#region IProduction implementation
+
+	/// <inheritdoc />
+	[AllowNull]
+	public string Title { get; set; }
+
+	/// <inheritdoc />
+	[AllowNull]
+	public string Overview { get; set; }
+
+	/// <inheritdoc />
+	public DateTime ReleaseDate { get; set; }
+
+	/// <inheritdoc />
+	[AllowNull]
+	public string OriginalLanguage { get; set; }
+
+	#endregion IProduction implementation
+
+	#region IPopularity implementation
+
+	/// <inheritdoc />
+	public double Popularity { get; set; }
+
+	/// <inheritdoc />
+	public long VoteCount { get; set; }
+
+	/// <inheritdoc />
+	public double VoteAverage { get; set; }
+
+	#endregion IPopularity implementation
+
+	#region IPublicity implementation
+
+	/// <inheritdoc />
+	[AllowNull]
+	public string PosterUrl { get; set; }
+
+	#endregion IPublicity implementation
+
+	[AllowNull]
+	public string Genres { get; set; }
+}
