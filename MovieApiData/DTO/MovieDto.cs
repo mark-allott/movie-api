8e78cfd54f67a76a73692b050d66bbea3e76b866
malsoft <github@malsoft.com>
2023-06-05 00:07:00 +0100

    Defines DTOs for the genre and movie types

---
+++ MovieApiData/DTO/MovieDto.cs
@@ -0,0 +1,47 @@
+﻿using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.DTO
+{
+	public class MovieDto :
+		AbstractDto,
+		IProduction,
+		IPopularity,
+		IPublicity
+	{
+		#region IProduction implementation
+
+		/// <inheritdoc />
+		public string Title { get; set; } = string.Empty;
+
+		/// <inheritdoc />
+		public string Overview { get; set; } = string.Empty;
+
+		/// <inheritdoc />
+		public DateTime ReleaseDate { get; set; }
+
+		/// <inheritdoc />
+		public string OriginalLanguage { get; set; } = string.Empty;
+
+		#endregion IProduction implementation
+
+		#region IPopularity implementation
+
+		/// <inheritdoc />
+		public double Popularity { get; set; }
+
+		/// <inheritdoc />
+		public long VoteCount { get; set; }
+
+		/// <inheritdoc />
+		public double VoteAverage { get; set; }
+
+		#endregion IPopularity implementation
+
+		#region IPublicity implementation
+
+		/// <inheritdoc />
+		public string? PosterUrl { get; set; }
+
+		#endregion IPublicity implementation
+	}
+}
