c9c72d31b7710c2cb712b7fd0a53c976ec9d2d8c
malsoft <github@malsoft.com>
2023-06-05 10:17:07 +0100

    Add interface for a unit of work

---
+++ MovieApiData/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿namespace MovieApi.Data.Interfaces
+{
+	public interface IUnitOfWork
+	{
+		/// <summary>
+		/// Commits changes to the context to the DB
+		/// </summary>
+		/// <returns></returns>
+		int SaveChanges();
+
+		/// <summary>
+		/// Async version of <see cref="SaveChanges"/>
+		/// </summary>
+		/// <param name="token"></param>
+		/// <returns></returns>
+		Task<int> SaveChangesAsync(CancellationToken token = default);
+
+		#region Transaction support
+
+		/// <summary>
+		/// Starts a transaction scope
+		/// </summary>
+		/// <returns>The scoped transaction</returns>
+		/// <remarks>Any commits/rollbacks are up to the consumer of the scoped transaction!</remarks>
+		ITransactionScope BeginTransaction();
+
+		/// <summary>
+		/// Async version of <see cref="BeginTransaction"/>
+		/// </summary>
+		/// <param name="token"></param>
+		/// <returns></returns>
+		Task<ITransactionScope> BeginTransactionAsync(CancellationToken token = default);
+
+		#endregion Transaction support
+	}
+}
