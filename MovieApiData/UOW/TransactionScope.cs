fcef90988780392102ca889511fb62a22d88903c
malsoft <github@malsoft.com>
2023-06-05 10:23:49 +0100

    Add concrete implementation of ITransactionScope

---
+++ MovieApiData/UOW/TransactionScope.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.UOW
+{
+	internal sealed class TransactionScope :
+		ITransactionScope
+	{
+		private readonly IDbContextTransaction _dbContextTransaction;
+
+		#region Ctor
+
+		internal TransactionScope(IDbContextTransaction dbContextTransaction)
+		{
+			_dbContextTransaction = dbContextTransaction ?? throw new ArgumentNullException(nameof(dbContextTransaction));
+		}
+
+		#endregion Ctor
+
+		#region ITransactionScope implementation
+
+		#region IDisposable implementation
+
+		/// <inheritdoc />
+		public void Dispose()
+		{
+			_dbContextTransaction.Dispose();
+		}
+
+		#endregion IDisposable implementation
+
+		/// <inheritdoc />
+		public void Commit()
+		{
+			_dbContextTransaction.Commit();
+		}
+
+		/// <inheritdoc />
+		public void Rollback()
+		{
+			_dbContextTransaction.Rollback();
+		}
+
+		/// <inheritdoc />
+		public Task CommitAsync(CancellationToken token = default)
+		{
+			return _dbContextTransaction.CommitAsync(token);
+		}
+
+		/// <inheritdoc />
+		public Task RollbackAsync(CancellationToken token = default)
+		{
+			return _dbContextTransaction.RollbackAsync(token);
+		}
+
+		#endregion ITransactionScope implementation
+	}
+}
