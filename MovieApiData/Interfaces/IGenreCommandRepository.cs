ec20c417f42dc163b210ef2ad41e1253f187b5d5
malsoft <github@malsoft.com>
2023-06-05 11:55:09 +0100

    Strongly-typed interfaces for Genre repos

---
+++ MovieApiData/Interfaces/IGenreCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieApi.Data.Entities;
+
+namespace MovieApi.Data.Interfaces
+{
+	/// <summary>
+	/// Strongly-typed version of <see cref="ICommandRepository{TEntity}"/> to entity type <see cref="Genre"/>
+	/// </summary>
+	public interface IGenreCommandRepository :
+		ICommandRepository<Genre>
+	{
+	}
+}
--- MovieApiData/Interfaces/IGenreQueryRepository.cs
+++ MovieApiData/Interfaces/IGenreQueryRepository.cs
@@ -2,6 +2,9 @@
 
 namespace MovieApi.Data.Interfaces
 {
+	/// <summary>
+	/// Strongly-typed version of <see cref="IQueryRepository{TEntity}"/> to entity type <see cref="Genre"/>
+	/// </summary>
 	public interface IGenreQueryRepository :
 		IQueryRepository<Genre>
 	{
