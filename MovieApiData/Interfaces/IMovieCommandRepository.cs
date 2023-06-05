18130952d8f4de41a67a9f8216d6734ea321a855
malsoft <github@malsoft.com>
2023-06-05 11:58:39 +0100

    Add strongly-typed interfaces for Movie repos

---
+++ MovieApiData/Interfaces/IMovieCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieApi.Data.Entities;
+
+namespace MovieApi.Data.Interfaces
+{
+	/// <summary>
+	/// Strongly-typed version of <see cref="ICommandRepository{TEntity}"/> to entity type <see cref="Movie"/>
+	/// </summary>
+	public interface IMovieCommandRepository :
+		ICommandRepository<Movie>
+	{
+	}
+}
--- MovieApiData/Interfaces/IMovieQueryRepository.cs
+++ MovieApiData/Interfaces/IMovieQueryRepository.cs
@@ -2,6 +2,9 @@
 
 namespace MovieApi.Data.Interfaces
 {
+	/// <summary>
+	/// Strongly-typed version of <see cref="IQueryRepository{TEntity}"/> to entity type <see cref="Movie"/>
+	/// </summary>
 	public interface IMovieQueryRepository :
 		IQueryRepository<Movie>
 	{
