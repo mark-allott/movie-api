4bf20c362a52f291cbc47757f93583dc0aaf62a1
malsoft <github@malsoft.com>
2023-06-06 16:46:39 +0100

    Defines an interface to identify services that can be auto-registered

---
+++ MovieApiData/Interfaces/IAutoRegisterService.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Data.Interfaces
+{
+	/// <summary>
+	/// provides a basic interface that allows searching for services that will auto-register themselves
+	/// </summary>
+	public interface IAutoRegisterService
+	{
+	}
+}
