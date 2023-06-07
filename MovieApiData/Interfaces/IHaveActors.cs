dd231afcd013760ca76f4193ea37ce62ff6f5ba6
malsoft <github@malsoft.com>
2023-06-08 00:55:03 +0100

    Add interface for an Actoirs property

---
+++ MovieApiData/Interfaces/IHaveActors.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Data.Interfaces;
+
+public interface IHaveActors
+{
+	/// <summary>
+	/// Provides a property to store a comma separated list of actors for the production
+	/// </summary>
+	string Actors { get; set; }
+}
