e6026cae0c963c24d12c200a52c59de966ca7a33
malsoft <github@malsoft.com>
2023-06-07 12:13:59 +0100

    Add request class for searching by title and genre

---
+++ MovieApiData/DTO/MovieSearchByTitleAndGenreRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieApi.Data.DTO
+{
+	/// <summary>
+	/// Defines a request object for searching for movies by title and genre(s)
+	/// </summary>
+	public class MovieSearchByTitleAndGenreRequest :
+		MovieSearchByTitleRequest
+	{
+		public string[] Genres { get; set; } = Array.Empty<string>();
+	}
+}
