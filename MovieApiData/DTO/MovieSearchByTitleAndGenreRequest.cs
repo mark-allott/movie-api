c8376027d9dd2407f562b3173117487d46e23b3e
malsoft <github@malsoft.com>
2023-06-07 15:06:37 +0100

    Add ability to filter for all or any of specified genres

--- MovieApiData/DTO/MovieSearchByTitleAndGenreRequest.cs
+++ MovieApiData/DTO/MovieSearchByTitleAndGenreRequest.cs
@@ -6,6 +6,18 @@
 	public class MovieSearchByTitleAndGenreRequest :
 		MovieSearchByTitleRequest
 	{
+		/// <summary>
+		/// Allows consumers to specify a list of genres to be matched with the title
+		/// </summary>
 		public string[] Genres { get; set; } = Array.Empty<string>();
+
+		/// <summary>
+		/// Allows consumers to specify whether the genres match any of those listed, or all of those listed
+		/// </summary>
+		/// <remarks>
+		/// Equivalent to performing a SQL <c>IN</c> operation when <c>false</c>, or <c>AND</c> when <c>true</c>
+		/// </remarks>
+		public bool MatchAllGenres { get; set; } = false;
+
 	}
 }
