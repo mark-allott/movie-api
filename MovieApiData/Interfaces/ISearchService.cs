ff2c36aec396b8af418d289e4a90af21786b6a7a
malsoft <github@malsoft.com>
2023-06-07 01:00:05 +0100

    Update SearchByTitle method to include useful params and return a search result class

--- MovieApiData/Interfaces/ISearchService.cs
+++ MovieApiData/Interfaces/ISearchService.cs
@@ -1,4 +1,6 @@
-﻿namespace MovieApi.Data.Interfaces
+﻿using MovieApi.Data.DTO;
+
+namespace MovieApi.Data.Interfaces
 {
 	public interface ISearchService :
 		IAutoRegisterService
@@ -7,8 +9,16 @@
 		/// Performs a search of the movie data by title
 		/// </summary>
 		/// <param name="title">The title, or partial title of the movie</param>
-		/// <returns></returns>
-		IEnumerable<object> SearchByTitle(string title);
+		/// <param name="pageNumber">Allows skipping pages of information in the returned results</param>
+		/// <param name="pageSize">Allows defining a size of the page of results</param>
+		/// <param name="useSqlLike"></param>
+		/// <returns>
+		/// A <see cref="MovieSearchResultCollection"/> object containing the results of the search
+		/// </returns>
+		/// <remarks>
+		/// A <paramref name="pageSize"/> value of 0 (default) is an indication to return all possible results
+		/// </remarks>
+		MovieSearchResultCollection SearchByTitle(string title, int pageNumber = 1, int pageSize = 0, bool useSqlLike = false);
 
 		/// <summary>
 		/// Performs a search of the movie data by genre(s)
