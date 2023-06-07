4acd3c85c19e7bde53b0bbb70e0aa48c61bd0d99
malsoft <github@malsoft.com>
2023-06-08 00:13:58 +0100

    Add definition for MovieActor link table

--- MovieApiData/Context/MovieContext.cs
+++ MovieApiData/Context/MovieContext.cs
@@ -26,6 +26,16 @@ namespace MovieApi.Data.Context
 				.HasMany(m => m.Genres)
 				.WithMany(m => m.Movies)
 				.UsingEntity<MovieGenre>(j => j.HasKey(nameof(Entities.MovieGenre.MovieId), nameof(Entities.MovieGenre.GenreId)));
+
+			//	MovieActor is a link table - create a hint for EF to use a compound PK using the two FKs it will use
+			modelBuilder.Entity<MovieActor>()
+				.HasKey(k => new { k.MovieId, k.ActorId });
+
+			//	Define the associations between Movie <-> MovieActor <-> Actor
+			modelBuilder.Entity<Movie>()
+				.HasMany(m => m.Actors)
+				.WithMany(m => m.Movies)
+				.UsingEntity<MovieActor>(j => j.HasKey(nameof(Entities.MovieActor.MovieId), nameof(Entities.MovieActor.ActorId)));
 		}
 
 		#region IMovieContext implementation
@@ -45,5 +55,10 @@ namespace MovieApi.Data.Context
 		///	Definition for the many-to-many link table between Movies and Genres
 		/// </summary>
 		public DbSet<MovieGenre>? MovieGenre { get; set; }
+
+		/// <summary>
+		///	Definition for the many-to-many link table between Movies and Actors
+		/// </summary>
+		public DbSet<MovieActor>? MovieActor { get; set; }
 	}
 }
