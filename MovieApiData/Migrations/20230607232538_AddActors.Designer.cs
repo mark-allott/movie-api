daa3c3cf992c457db42669c980b8e1c3c2555d04
malsoft <github@malsoft.com>
2023-06-08 00:30:46 +0100

    Add migration for Actors

--- MovieApiData/Migrations/20230607232538_AddActors.Designer.cs
+++ MovieApiData/Migrations/20230607232538_AddActors.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieApi.Data.Context;
 
@@ -11,9 +12,11 @@ using MovieApi.Data.Context;
 namespace MovieApi.Data.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20230607232538_AddActors")]
+    partial class AddActors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
