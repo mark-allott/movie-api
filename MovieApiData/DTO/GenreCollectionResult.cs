5f177c804cab1545ab0e1eb61c2f121d79e0524f
malsoft <github@malsoft.com>
2023-06-07 09:05:17 +0100

    Add concrete class for returning list of genres

---
+++ MovieApiData/DTO/GenreCollectionResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApi.Data.DTO;
+
+public class GenreCollectionResult :
+	AbstractSearchResultCollection<string>
+{
+	public GenreCollectionResult(IEnumerable<string> results, int totalResults, int pageNumber, int pageSize) :
+		base(results, totalResults, pageNumber, pageSize)
+	{
+	}
+}
