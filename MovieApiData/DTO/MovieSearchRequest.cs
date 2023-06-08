80d01c5b6a80b1c802815a4bc737125342a38541
malsoft <github@malsoft.com>
2023-06-08 08:46:56 +0100

    Add search request class

---
+++ MovieApiData/DTO/MovieSearchRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieApi.Data.DTO
+{
+	/// <summary>
+	/// Defines a request object for searching for movies by title, genre(s) amd actor(s)
+	/// </summary>
+	public class MovieSearchRequest :
+		MovieSearchByTitleAndGenreRequest
+	{
+		/// <summary>
+		/// Allows consumers to specify a list of actors to be matched with the title
+		/// </summary>
+		public string[] Actors { get; set; } = Array.Empty<string>();
+
+		/// <summary>
+		/// Allows consumers to specify whether the actors match any of those listed, or all of those listed
+		/// </summary>
+		/// <remarks>
+		/// Equivalent to performing a SQL <c>IN</c> operation when <c>false</c>, or <c>AND</c> when <c>true</c>
+		/// </remarks>
+		public bool MatchAllActors { get; set; } = false;
+
+	}
+}
