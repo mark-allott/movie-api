7822b709cde1824182ed6f0d8cb1d406c7b7e192
malsoft <github@malsoft.com>
2023-06-08 09:02:13 +0100

    Add query repo for Actor

---
+++ MovieApiData/Repositories/ActorQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieApi.Data.Context;
+using MovieApi.Data.Entities;
+using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.Repositories
+{
+	public class ActorQueryRepository :
+		AbstractQueryRepository<MovieContext, Actor>,
+		IActorQueryRepository
+	{
+		public ActorQueryRepository(MovieContext context) :
+			base(context)
+		{
+		}
+	}
+}
