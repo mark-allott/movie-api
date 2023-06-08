6410bc577e99af956cc464018ef27fde2928d834
malsoft <github@malsoft.com>
2023-06-08 09:03:55 +0100

    Add full repo for Actor

---
+++ MovieApiData/Interfaces/IActorRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieApi.Data.Entities;
+
+namespace MovieApi.Data.Interfaces
+{
+	/// <summary>
+	/// Combination of the <see cref="ICommandRepository{TEntity}"/> and <see
+	/// cref="IQueryRepository{TEntity}"/> interfaces, strongly-typed to entity <see cref="Actor"/>
+	/// </summary>
+	public interface IActorRepository :
+		ICommandRepository<Actor>,
+		IQueryRepository<Actor>
+	{
+	}
+}
