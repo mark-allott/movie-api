12a51cb698c8079d872763ee209e5dc257188bdb
malsoft <github@malsoft.com>
2023-06-08 08:51:05 +0100

    Add result class for actor query

---
+++ MovieApiData/DTO/ActorResultCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApi.Data.DTO;
+
+public class ActorResultCollection :
+	AbstractSearchResultCollection<string>
+{
+	public ActorResultCollection(IEnumerable<string> results, int totalResults, int pageNumber, int pageSize) :
+		base(results, totalResults, pageNumber, pageSize)
+	{
+	}
+}
