77d0c189e661a7c2afddbd930382fe990d155666
malsoft <github@malsoft.com>
2023-06-08 09:00:17 +0100

    Add command repo for Actor

---
+++ MovieApiData/Repositories/ActorCommandRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieApi.Data.Context;
+using MovieApi.Data.Entities;
+using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.Repositories
+{
+	public class ActorCommandRepository :
+		AbstractCommandRepository<MovieContext, Actor>,
+		IActorCommandRepository
+	{
+		public ActorCommandRepository(MovieContext context) :
+			base(context)
+		{
+		}
+	}
+}
