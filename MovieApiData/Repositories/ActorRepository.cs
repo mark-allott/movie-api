47d13f8166e4235e98dfcf9b7491290e41d454a0
malsoft <github@malsoft.com>
2023-06-08 09:03:55 +0100

    Add full repo for Actor

---
+++ MovieApiData/Repositories/ActorRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieApi.Data.Context;
+using MovieApi.Data.Entities;
+using MovieApi.Data.Interfaces;
+
+namespace MovieApi.Data.Repositories
+{
+	public class ActorRepository :
+		AbstractRepository<MovieContext, Actor>,
+		IActorRepository
+	{
+		public ActorRepository(MovieContext context) :
+			base(context)
+		{
+		}
+	}
+}
