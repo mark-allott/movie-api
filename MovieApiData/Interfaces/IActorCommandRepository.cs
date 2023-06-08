3211189b6de0ef723884f78cfeff1c54325b059a
malsoft <github@malsoft.com>
2023-06-08 09:00:17 +0100

    Add command repo for Actor

---
+++ MovieApiData/Interfaces/IActorCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieApi.Data.Entities;
+
+namespace MovieApi.Data.Interfaces
+{
+	/// <summary>
+	/// Strongly-typed version of <see cref="ICommandRepository{TEntity}"/> to entity type <see cref="Actor"/>
+	/// </summary>
+	public interface IActorCommandRepository :
+		ICommandRepository<Actor>
+	{
+	}
+}
